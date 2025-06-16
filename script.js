fb6da11d8c2b661606cc433d548875fbb6f09adc
Creed <o.frank@tajnetworkafrica.com>
2025-06-16 20:50:37 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,173 @@
+/*
+JS Class 1
+
+// alert("How was class today?");
+
+// console.log("Temitope is my Friend");
+
+// console.log(40 + 8 + 23 - 10);
+
+// console.log(5 ** 5);
+
+// alert("I want this instead");
+
+// console.log("Adams");
+// console.log(55);
+// console.log(55 - 20);
+// console.log(true);
+// console.log(false);
+
+// console.log("Temitope is my Friend");
+
+// Declaring a variable
+const PI = 3.142;
+// let GRAVITY = 9.81;
+
+let num1 = 50; // this is a number data type
+let first_name = "Adams"; // this is a string value
+let last_name = "Abdul";
+let firstNamePerson = "Kareema";
+let first_name_person = "Kareema";
+let $num_1 = 20;
+
+let year2020 = "Covid Year";
+let year_2023 = "Election Year";
+let $num2 = 34;
+
+let firstName = "Kwame";
+let lastName = "Mensah";
+let country = "Ghana";
+let city = "Kumasi";
+let age = 50;
+let isMarried = true; // boolean value
+
+console.log(firstName); // This is my first name
+console.log(lastName);
+console.log(country);
+console.log(city);
+console.log(age);
+console.log(isMarried);
+
+console.log(firstName, lastName, country, city, age, isMarried);
+
+let gravity = 9.81; // this is a const variable for gravity
+const boilingPoint = 100;
+console.log(gravity, boilingPoint, PI);
+
+// COMMENT
+
+// Single Line
+// Multi-line
+// third line
+
+*/
+// Let
+let age = 20; // declaring a variable
+// console.log(age);
+
+age = 21; // reassigning a variable or mutating a variable
+console.log(age);
+
+let noOfChildren; // empty variable
+
+noOfChildren = 5;
+// console.log(noOfChildren);
+
+let fruit = "Mango";
+// console.log(fruit);
+
+fruit = "Apple";
+// console.log(fruit);
+
+// Const
+const city = "Nairobi";
+// console.log(city);
+
+const yearOfBirth = 2000;
+const pi = 3.142;
+
+// Var
+var course = "javascript";
+course = "python";
+// console.log(course);
+
+// var firstName = "Alice";
+// var firstName = "Bob";
+
+// console.log(firstName);
+
+// Data types
+// Primitive
+// Number
+
+// Strings
+const empty = "";
+console.log(empty);
+
+const a = "A";
+const b = "JavaScript is an 'amazing' language";
+// console.log(b);
+// below esc key
+const c = `We can also create a string using a backtick`;
+console.log(c);
+
+const d = "5";
+console.log(d);
+
+// Booleans
+const yes = true;
+const no = false;
+console.log(yes);
+
+console.log(3 > 2);
+console.log(3 > 10);
+
+// Undefined
+let lastName;
+console.log(lastName);
+
+lastName = "Isaac";
+console.log(lastName);
+
+// Null
+let emptyValue = null;
+console.log(emptyValue);
+
+// Non-Primitive or Object
+
+// array
+const listOfFruits = [
+  "mango",
+  "apple",
+  "grapes",
+  "banana",
+  "banana",
+  "banana",
+  "banana",
+  "banana",
+  "banana",
+];
+console.log(listOfFruits);
+
+// object
+const myObject = {
+  firstName: "Annionting", // string
+  LastName: "Chidi", // string
+  age: 20, // number
+  gender: "female", // string
+  isMarried: false, // boolean
+  hobbies: ["music", "football", "reading"],
+};
+console.log(myObject);
+
+console.log(typeof "victor"); // string
+console.log(typeof 90); // number
+console.log(typeof true); // boolean
+console.log(typeof null); // null or undefined
+console.log(typeof undefined); // undefined
+console.log(typeof {}); // object
+console.log(typeof []); // object
+console.log(typeof (2 > 4)); // booloean
+
+// control structure
+// if () {}
